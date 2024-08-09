b402c7a452943156b01adbfa98223a57aa458de4
wagngomes gomes <wagner.gomes.cx2@gmail.com>
2024-08-09 01:05:38 +0000

    feat: multerConfig ok

---
+++ src/lib/multerConfig.ts
@@ -0,0 +1,25 @@
+import multer from 'multer'
+import path from 'path'
+import fs from 'fs'
+
+// Caminho absoluto para o diretório de uploads
+const uploadDir: string = path.resolve(__dirname, './uploads');
+
+// Certifique-se de que o diretório de uploads existe
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+}
+
+const storage: multer.StorageEngine = multer.diskStorage({
+    destination: (req, file, callback) => {
+        callback(null, uploadDir);
+    },
+    filename: (req, file, callback) => {
+        const time = Date.now(); // Use Date.now() para obter um timestamp
+        callback(null, `${time}_${file.originalname}`);
+    }
+});
+
+module.exports = storage;
+
+
